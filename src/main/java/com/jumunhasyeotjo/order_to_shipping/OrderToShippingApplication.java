e2d981504c0724771d3554cc7eedf1ea57f66392
mmihye <smilehye910@naver.com>
2025-11-05 17:58:46 +0900

    feat : 프로젝트 초기 생성

---
+++ src/main/java/com/jumunhasyeotjo/order_to_shipping/OrderToShippingApplication.java
@@ -0,0 +1,13 @@
+package com.jumunhasyeotjo.order_to_shipping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderToShippingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderToShippingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jumunhasyeotjo/order_to_shipping/OrderToShippingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jumunhasyeotjo.order_to_shipping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderToShippingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
