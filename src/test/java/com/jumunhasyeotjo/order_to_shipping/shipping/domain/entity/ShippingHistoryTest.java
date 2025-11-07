78070854934c9474f900bbcf2a27ddf23681ebfc
mmihye <smilehye910@naver.com>
2025-11-07 23:27:30 +0900

    feat : 배송기록 도메인 개발 및 테스트 코드 작성

--- src/test/java/com/jumunhasyeotjo/order_to_shipping/shipping/domain/entity/ShippingHistoryTest.java
+++ src/test/java/com/jumunhasyeotjo/order_to_shipping/shipping/domain/entity/ShippingHistoryTest.java
@@ -1,15 +1,20 @@
 package com.jumunhasyeotjo.order_to_shipping.shipping.domain.entity;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.UUID;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
 
 import com.jumunhasyeotjo.order_to_shipping.common.exception.BusinessException;
+import com.jumunhasyeotjo.order_to_shipping.shipping.domain.vo.PhoneNumber;
 import com.jumunhasyeotjo.order_to_shipping.shipping.domain.vo.RouteInfo;
+import com.jumunhasyeotjo.order_to_shipping.shipping.domain.vo.ShippingAddress;
 import com.jumunhasyeotjo.order_to_shipping.shipping.domain.vo.ShippingHistoryStatus;
+import com.jumunhasyeotjo.order_to_shipping.shipping.domain.vo.ShippingStatus;
 import com.jumunhasyeotjo.order_to_shipping.shipping.fixtures.ShippingFixture;
 import com.jumunhasyeotjo.order_to_shipping.shipping.fixtures.ShippingHistoryFixture;
 
@@ -57,7 +62,7 @@ class ShippingHistoryTest {
 	void changeDriver_WhenStatusIsNotWaiting_ShouldThrowException() {
 		//given
 		ShippingHistory shippingHistory = ShippingHistoryFixture.createDefault();
-		shippingHistory.markAsDeparted();
+		shippingHistory.departDelivery();
 		UUID newDriverId = UUID.randomUUID();
 
 		// when & then
@@ -74,7 +79,7 @@ class ShippingHistoryTest {
 		Integer actualDistance = 120;
 
 		// when & then
-		assertThatThrownBy(() -> shippingHistory.markAsArrived(actualDistance))
+		assertThatThrownBy(() -> shippingHistory.arriveDelivery(actualDistance))
 			.isInstanceOf(BusinessException.class)
 			.hasMessageContaining("해당 상태로 전환할 수 없습니다.");
 	}
@@ -86,7 +91,7 @@ class ShippingHistoryTest {
 		ShippingHistory shippingHistory = ShippingHistoryFixture.createDefault();
 
 		//when
-		shippingHistory.markAsDeparted();
+		shippingHistory.departDelivery();
 
 		//then
 		assertThat(shippingHistory.getStatus()).isEqualTo(ShippingHistoryStatus.MOVING_TO_HUB);
@@ -98,13 +103,13 @@ class ShippingHistoryTest {
 	void arriveDelivery_ShouldChangeStatusToArrivedAndSaveActualRouteInfo() throws InterruptedException {
 		//given
 		ShippingHistory shippingHistory = ShippingHistoryFixture.createDefault();
-		shippingHistory.markAsDeparted();
+		shippingHistory.departDelivery();
 		Integer actualDistance = 120;
 
 		Thread.sleep(60000); // 배송시간 지연
 
 		//when
-		shippingHistory.markAsArrived(120);
+		shippingHistory.arriveDelivery(120);
 
 		//then
 		assertThat(shippingHistory.getStatus()).isEqualTo(ShippingHistoryStatus.ARRIVED);
