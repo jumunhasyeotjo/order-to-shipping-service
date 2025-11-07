8f11ed8f66091675879e9ce3eba40ed348ed50d5
mmihye <smilehye910@naver.com>
2025-11-07 23:27:30 +0900

    feat : 배송기록 도메인 개발 및 테스트 코드 작성

---
+++ src/test/java/com/jumunhasyeotjo/order_to_shipping/shipping/fixtures/ShippingHistoryFixture.java
@@ -0,0 +1,23 @@
+package com.jumunhasyeotjo.order_to_shipping.shipping.fixtures;
+
+import java.util.UUID;
+
+import com.jumunhasyeotjo.order_to_shipping.shipping.domain.entity.Shipping;
+import com.jumunhasyeotjo.order_to_shipping.shipping.domain.entity.ShippingHistory;
+import com.jumunhasyeotjo.order_to_shipping.shipping.domain.vo.PhoneNumber;
+import com.jumunhasyeotjo.order_to_shipping.shipping.domain.vo.RouteInfo;
+import com.jumunhasyeotjo.order_to_shipping.shipping.domain.vo.ShippingAddress;
+
+public class ShippingHistoryFixture {
+
+	public static ShippingHistory createDefault(){
+		Shipping shipping = ShippingFixture.createDefault();
+		UUID driverId = UUID.randomUUID();
+		Integer sequence = 1;
+		String origin = "출발지";
+		String destination = "도착지";
+		RouteInfo expectRouteInfo = RouteInfo.of(100, 100);
+
+		return ShippingHistory.create(shipping, driverId, sequence, origin, destination, expectRouteInfo);
+	}
+}
