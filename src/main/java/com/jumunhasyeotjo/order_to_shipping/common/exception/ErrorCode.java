fe3cdf4fb4a887b5d1a8354d7d8a3b4e13827d30
mmihye <smilehye910@naver.com>
2025-11-07 11:07:15 +0900

    fix : 에러 코드 순차적으로 변경

--- src/main/java/com/jumunhasyeotjo/order_to_shipping/common/exception/ErrorCode.java
+++ src/main/java/com/jumunhasyeotjo/order_to_shipping/common/exception/ErrorCode.java
@@ -8,24 +8,24 @@ import lombok.Getter;
 public enum ErrorCode {
 
     // 400 BAD_REQUEST
-    INVALID_INPUT(HttpStatus.BAD_REQUEST, "E002", "잘못된 요청입니다."),
-    CREATE_VALIDATE_EXCEPTION(HttpStatus.BAD_REQUEST,"E005", "객체 생성에 실패했습니다."),
-    VALIDATION_FAILED(HttpStatus.BAD_REQUEST, "E006", "입력값 검증에 실패했습니다."),
-    INVALID_JSON(HttpStatus.BAD_REQUEST, "E007",  "잘못된 JSON 형식입니다."),
-    REQUIRED_VALUE_MISSING(HttpStatus.BAD_REQUEST, "E008", "필수 값이 누락되었습니다."),
-    INVALID_FORMAT(HttpStatus.BAD_REQUEST, "E009", "잘못된 입력 형식입니다."),
-    INVALID_STATUS_TRANSITION(HttpStatus.BAD_REQUEST, "E010", "해당 상태로 전환할 수 없습니다."),
-    HUB_ORIGIN_EQUALS_DESTINATION(HttpStatus.BAD_REQUEST, "E011", "도착 허브와 출발 허브가 같을 수 없습니다."),
-    INVALID_STATE_FOR_MODIFICATION(HttpStatus.BAD_REQUEST, "E012", "현재 상태에서는 정보를 수정할 수 없습니다."),
+    INVALID_INPUT(HttpStatus.BAD_REQUEST, "E001", "잘못된 요청입니다."),
+    CREATE_VALIDATE_EXCEPTION(HttpStatus.BAD_REQUEST,"E002", "객체 생성에 실패했습니다."),
+    VALIDATION_FAILED(HttpStatus.BAD_REQUEST, "E003", "입력값 검증에 실패했습니다."),
+    INVALID_JSON(HttpStatus.BAD_REQUEST, "E004",  "잘못된 JSON 형식입니다."),
+    REQUIRED_VALUE_MISSING(HttpStatus.BAD_REQUEST, "E005", "필수 값이 누락되었습니다."),
+    INVALID_FORMAT(HttpStatus.BAD_REQUEST, "E006", "잘못된 입력 형식입니다."),
+    INVALID_STATUS_TRANSITION(HttpStatus.BAD_REQUEST, "E007", "해당 상태로 전환할 수 없습니다."),
+    HUB_ORIGIN_EQUALS_DESTINATION(HttpStatus.BAD_REQUEST, "E008", "도착 허브와 출발 허브가 같을 수 없습니다."),
+    INVALID_STATE_FOR_MODIFICATION(HttpStatus.BAD_REQUEST, "E009", "현재 상태에서는 정보를 수정할 수 없습니다."),
 
     // 401 UNAUTHORIZED
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "E003", "인증이 필요합니다."),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "E010", "인증이 필요합니다."),
 
     // 403 FORBIDDEN
-    FORBIDDEN(HttpStatus.FORBIDDEN, "E004", "접근이 거부되었습니다."),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "E011", "접근이 거부되었습니다."),
 
     // 500 INTERNAL_SERVER_ERROR
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "E001", "서버 에러가 발생했습니다.");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "E012", "서버 에러가 발생했습니다.");
 
     private final HttpStatus status;
     private final String code;
