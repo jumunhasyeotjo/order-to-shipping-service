6ff518d8c6a9fb0241f07d81f6e835c5d0c21f22
RaeHwan Park <prh0108@naver.com>
2025-11-10 07:39:43 +0900

    feat: open feign 추가

--- src/main/java/com/jumunhasyeotjo/order_to_shipping/OrderToShippingApplication.java
+++ src/main/java/com/jumunhasyeotjo/order_to_shipping/OrderToShippingApplication.java
@@ -2,8 +2,10 @@ package com.jumunhasyeotjo.order_to_shipping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class OrderToShippingApplication {
 
 	public static void main(String[] args) {
