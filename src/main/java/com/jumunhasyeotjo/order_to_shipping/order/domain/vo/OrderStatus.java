e1c316203e2940e90e4638b132eebfe844957d78
RaeHwan Park <prh0108@naver.com>
2025-11-11 01:29:42 +0900

    feat: 주문 상태 description 추가

--- src/main/java/com/jumunhasyeotjo/order_to_shipping/order/domain/vo/OrderStatus.java
+++ src/main/java/com/jumunhasyeotjo/order_to_shipping/order/domain/vo/OrderStatus.java
@@ -1,7 +1,20 @@
 package com.jumunhasyeotjo.order_to_shipping.order.domain.vo;
 
+import lombok.Getter;
+
+@Getter
 public enum OrderStatus {
-    PENDING, PAYED, SHIPPED, DONE, CANCELLED;
+    PENDING("결제 대기"),
+    PAYED("결제 완료"),
+    SHIPPED("배송중"),
+    DONE("배송 완료"),
+    CANCELLED("주문 취소");
+
+    private final String description;
+
+    OrderStatus(String description) {
+        this.description = description;
+    }
 
     public boolean canUpdateOrder() {
         return this.equals(PENDING);
