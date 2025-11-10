1784c82e697826ccaf38edba6baae9ac56f06161
Mihye <92644651+mmihye@users.noreply.github.com>
2025-11-10 09:59:34 +0900

    [Feature/#6] 배송기록 도메인 개발 (#7)
    * feat : 배송기록 도메인 개발 및 테스트 코드 작성
    
    * feat : 배송기록 상태에 CANCELD 추가
    
    * feat : 배송상태기록 취소
    
    * feat : 배송 도메인 서비스 개발

--- src/main/java/com/jumunhasyeotjo/order_to_shipping/shipping/domain/entity/ShippingHistory.java
+++ src/main/java/com/jumunhasyeotjo/order_to_shipping/shipping/domain/entity/ShippingHistory.java
@@ -1,14 +1,19 @@
 package com.jumunhasyeotjo.order_to_shipping.shipping.domain.entity;
 
+import static com.jumunhasyeotjo.order_to_shipping.common.exception.ErrorCode.*;
+
+import java.time.LocalDateTime;
 import java.util.UUID;
 
-import org.springframework.data.geo.Distance;
-
 import com.jumunhasyeotjo.order_to_shipping.common.entity.BaseEntity;
+import com.jumunhasyeotjo.order_to_shipping.common.exception.BusinessException;
+import com.jumunhasyeotjo.order_to_shipping.shipping.domain.vo.RouteInfo;
 import com.jumunhasyeotjo.order_to_shipping.shipping.domain.vo.ShippingHistoryStatus;
-import com.jumunhasyeotjo.order_to_shipping.shipping.domain.vo.ShippingStatus;
 
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.AttributeOverrides;
 import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -17,7 +22,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -50,13 +54,109 @@ public class ShippingHistory extends BaseEntity {
 	@Column(nullable = false)
 	private ShippingHistoryStatus status;
 
-	@Column(nullable = false)
-	private Integer expectDistance;
+	@Embedded
+	@AttributeOverrides({
+		@AttributeOverride(name = "distance", column = @Column(name = "expect_distance")),
+		@AttributeOverride(name = "interval", column = @Column(name = "expect_interval"))
+	})
+	private RouteInfo expectRouteInfo;
 
-	@Column(nullable = false)
-	private Integer expectInterval;
+	@Embedded
+	@AttributeOverrides({
+		@AttributeOverride(name = "distance", column = @Column(name = "actual_distance")),
+		@AttributeOverride(name = "interval", column = @Column(name = "actual_interval"))
+	})
+	private RouteInfo actualRouteInfo;
 
-	private Integer actualDistance;
+	private LocalDateTime departedAt;
 
-	private Integer actualInterval;
+	private LocalDateTime arrivedAt;
+
+
+	/**
+	 * 배송 생성 팩토리 메서드
+	 */
+	public static ShippingHistory create(Shipping shipping, UUID driverId, Integer sequence, String origin, String destination,
+		RouteInfo expectRouteInfo) {
+
+		validateCreateArgs(shipping, driverId, sequence, origin, destination, expectRouteInfo);
+
+		ShippingHistory shippingHistory = new ShippingHistory();
+		shippingHistory.shipping = shipping;
+		shippingHistory.status = ShippingHistoryStatus.WAITING;
+		shippingHistory.driverId = driverId;
+		shippingHistory.sequence = sequence;
+		shippingHistory.origin = origin;
+		shippingHistory.destination = destination;
+		shippingHistory.expectRouteInfo = expectRouteInfo;
+
+		return shippingHistory;
+	}
+
+	/**
+	 * 배송자 수정
+	 */
+	public void changeDriver(UUID driverId){
+		if(!this.status.equals(ShippingHistoryStatus.WAITING)){
+			throw new BusinessException(INVALID_STATE_FOR_MODIFICATION);
+		}
+
+		this.driverId = driverId;
+	}
+
+	/**
+	 * 배송 출발
+	 */
+	public void markAsDeparted(){
+		ShippingHistoryStatus newStatus = ShippingHistoryStatus.MOVING_TO_HUB;
+		if(!canTransitionTo(newStatus)){
+			throw new BusinessException(INVALID_STATUS_TRANSITION);
+		}
+
+		this.status = newStatus;
+		this.departedAt = LocalDateTime.now();
+	}
+
+	/**
+	 * 배송 도착
+	 */
+	public void markAsArrived(Integer actualDistance){
+		ShippingHistoryStatus newStatus = ShippingHistoryStatus.ARRIVED;
+		if(!canTransitionTo(newStatus)){
+			throw new BusinessException(INVALID_STATUS_TRANSITION);
+		}
+
+		this.status = newStatus;
+		this.arrivedAt = LocalDateTime.now();
+		this.actualRouteInfo = RouteInfo.ofActualRoute(actualDistance, this.departedAt, arrivedAt);
+	}
+
+	/**
+	 * 배송취소
+	 */
+	public void cancel(){
+		if(!this.status.equals(ShippingHistoryStatus.WAITING)){
+			throw new BusinessException(INVALID_STATE_FOR_MODIFICATION);
+		}
+
+		this.status = ShippingHistoryStatus.CANCELED;
+	}
+
+	private boolean canTransitionTo(ShippingHistoryStatus newStatus) {
+		return this.status.getSequence() + 1 == newStatus.getSequence();
+	}
+
+	private static void validateCreateArgs(Shipping shipping, UUID driverId, Integer sequence, String origin, String destination,
+		RouteInfo expectRouteInfo) {
+		if (shipping == null || driverId == null || sequence == null ||
+			origin == null || destination == null ||
+			expectRouteInfo == null || origin.isBlank() || destination.isBlank()) {
+			throw new BusinessException(REQUIRED_VALUE_MISSING);
+		}
+
+		if (sequence <= 0) {
+			throw new BusinessException(INVALID_INPUT);
+		}
+	}
+
 }
