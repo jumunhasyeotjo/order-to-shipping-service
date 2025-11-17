627e03ae8adee58ac563a752e5adb4af7bd7f1f4
mmihye <smilehye910@naver.com>
2025-11-17 12:50:43 +0900

    fix: gradle 잘못된 통합 수정

--- src/main/java/com/jumunhasyeotjo/order_to_shipping/shipping/application/command/ChangeStatusCommand.java
+++ src/main/java/com/jumunhasyeotjo/order_to_shipping/shipping/application/command/ChangeStatusCommand.java
@@ -3,7 +3,6 @@ package com.jumunhasyeotjo.order_to_shipping.shipping.application.command;
 import java.util.UUID;
 
 public record ChangeStatusCommand(
-	UUID shippingHistoryId,
-	String role
+	UUID shippingHistoryId
 ) {
 }
