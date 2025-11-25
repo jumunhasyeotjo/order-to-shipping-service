fcc58949396bb04f708d6a350bd84df1e52e66d5
mmihye <smilehye910@naver.com>
2025-11-25 15:34:45 +0900

    fix: redis 설정 변경

--- src/main/java/com/jumunhasyeotjo/order_to_shipping/common/config/RedisConfig.java
+++ src/main/java/com/jumunhasyeotjo/order_to_shipping/common/config/RedisConfig.java
@@ -3,12 +3,30 @@ package com.jumunhasyeotjo.order_to_shipping.common.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
-  @Bean
-  public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory cf) {
-    return new StringRedisTemplate(cf);
-  }
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        template.afterPropertiesSet();
+        return template;
+    }
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory connectionFactory) {
+        return new StringRedisTemplate(connectionFactory);
+    }
 }
