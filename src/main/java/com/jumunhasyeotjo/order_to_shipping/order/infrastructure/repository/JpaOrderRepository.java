6888b3039bba0037979278042a5ff95283d28dd5
RaeHwan Park <prh0108@naver.com>
2025-12-04 12:20:29 +0900

    feat: 주문내 공급 업체별 조회 기능 추가

--- src/main/java/com/jumunhasyeotjo/order_to_shipping/order/infrastructure/repository/JpaOrderRepository.java
+++ src/main/java/com/jumunhasyeotjo/order_to_shipping/order/infrastructure/repository/JpaOrderRepository.java
@@ -1,12 +1,14 @@
 package com.jumunhasyeotjo.order_to_shipping.order.infrastructure.repository;
 
 import com.jumunhasyeotjo.order_to_shipping.order.domain.entity.Order;
+import com.jumunhasyeotjo.order_to_shipping.order.presentation.dto.response.CompanyOrderItemsRes;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface JpaOrderRepository extends JpaRepository<Order, UUID>{
@@ -15,4 +17,9 @@ public interface JpaOrderRepository extends JpaRepository<Order, UUID>{
             "where o.receiverCompanyId = :companyId",
             countQuery = "select count(o) from Order o where o.receiverCompanyId = :companyId")
     Page<Order> findAllByCompanyId(@Param("companyId") UUID companyId, Pageable pageable);
+
+    @Query("SELECT new com.jumunhasyeotjo.order_to_shipping.order.presentation.dto.response.CompanyOrderItemsRes(op.productId, op.quantity) " +
+            "FROM OrderProduct op " +
+            "WHERE op.orderCompany.id = :companyOrderId")
+    List<CompanyOrderItemsRes> findAllByOrderCompany(UUID companyOrderId);
 }
--- src/main/java/com/jumunhasyeotjo/order_to_shipping/order/infrastructure/repository/OrderRepositoryAdapter.java
+++ src/main/java/com/jumunhasyeotjo/order_to_shipping/order/infrastructure/repository/OrderRepositoryAdapter.java
@@ -2,11 +2,13 @@ package com.jumunhasyeotjo.order_to_shipping.order.infrastructure.repository;
 
 import com.jumunhasyeotjo.order_to_shipping.order.domain.entity.Order;
 import com.jumunhasyeotjo.order_to_shipping.order.domain.repository.OrderRepository;
+import com.jumunhasyeotjo.order_to_shipping.order.presentation.dto.response.CompanyOrderItemsRes;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -27,6 +29,11 @@ public class OrderRepositoryAdapter implements OrderRepository {
     @Override
     public Page<Order> findAllByCompanyId(UUID companyId, Pageable pageable) {
         return jpaOrderRepository.findAllByCompanyId(companyId, pageable);
+    }
+
+    @Override
+    public List<CompanyOrderItemsRes> findAllByOrderCompany(UUID companyOrderId) {
+        return jpaOrderRepository.findAllByOrderCompany(companyOrderId);
     }
 
 }
