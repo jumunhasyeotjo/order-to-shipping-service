e13cb4388f8dd7e5cb85a1ca75d8ad5219f13380
RaeHwan Park <prh0108@naver.com>
2025-12-04 12:16:14 +0900

    feat: passport config 분리

--- src/main/java/com/jumunhasyeotjo/order_to_shipping/OrderToShippingApplication.java
+++ src/main/java/com/jumunhasyeotjo/order_to_shipping/OrderToShippingApplication.java
@@ -1,18 +1,8 @@
 package com.jumunhasyeotjo.order_to_shipping;
 
-import com.library.passport.config.WebMvcConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 
-@EnableFeignClients
-@ComponentScan(basePackages = {
-		"com.jumunhasyeotjo.order_to_shipping",
-		"com.library.passport"
-})
-@Import(WebMvcConfig.class)
 @SpringBootApplication
 public class OrderToShippingApplication {
 
