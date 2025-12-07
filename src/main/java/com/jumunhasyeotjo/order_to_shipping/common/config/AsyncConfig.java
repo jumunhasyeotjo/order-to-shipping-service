205242df450da051e09c8270832a810bcc96c643
RaeHwan Park <prh0108@naver.com>
2025-12-08 02:03:16 +0900

    feat: Configuration 누락 추가

--- src/main/java/com/jumunhasyeotjo/order_to_shipping/common/config/AsyncConfig.java
+++ src/main/java/com/jumunhasyeotjo/order_to_shipping/common/config/AsyncConfig.java
@@ -1,6 +1,7 @@
 package com.jumunhasyeotjo.order_to_shipping.common.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -9,6 +10,7 @@ import java.util.concurrent.Executor;
 
 @Slf4j
 @EnableAsync
+@Configuration
 public class AsyncConfig implements AsyncConfigurer {
 
     @Override
