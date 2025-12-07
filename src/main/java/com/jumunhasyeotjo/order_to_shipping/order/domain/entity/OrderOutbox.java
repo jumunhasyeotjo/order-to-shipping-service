c0d33ed456abcd297dfdacafbc06477b0e02e3f4
RaeHwan Park <prh0108@naver.com>
2025-12-08 01:42:41 +0900

    feat: Outbox 패턴 구현

---
+++ src/main/java/com/jumunhasyeotjo/order_to_shipping/order/domain/entity/OrderOutbox.java
@@ -0,0 +1,53 @@
+package com.jumunhasyeotjo.order_to_shipping.order.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "p_order_outbox")
+public class OrderOutbox {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String aggregateType;
+
+    @Column(nullable = false)
+    private UUID aggregateId;
+
+    @Column(nullable = false)
+    private String eventType;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String payload;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private boolean isPublished = false;
+
+    @Builder
+    public OrderOutbox(String aggregateType, UUID aggregateId, String eventType, String payload, LocalDateTime createdAt) {
+        this.aggregateType = aggregateType;
+        this.aggregateId = aggregateId;
+        this.eventType = eventType;
+        this.payload = payload;
+        this.createdAt = createdAt;
+    }
+
+    public void publish() {
+        this.isPublished = true;
+    }
+}
