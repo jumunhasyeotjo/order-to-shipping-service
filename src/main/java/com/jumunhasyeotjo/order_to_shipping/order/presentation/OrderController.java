5f060f85c1320436d462a76bf00fe2e6dbb7e329
RaeHwan Park <prh0108@naver.com>
2025-12-09 20:18:46 +0900

    refactor: controller 분리

--- src/main/java/com/jumunhasyeotjo/order_to_shipping/order/presentation/OrderController.java
+++ src/main/java/com/jumunhasyeotjo/order_to_shipping/order/presentation/OrderController.java
@@ -1,17 +1,21 @@
 package com.jumunhasyeotjo.order_to_shipping.order.presentation;
 
-import com.jumunhasyeotjo.order_to_shipping.common.ApiRes;
-import com.jumunhasyeotjo.order_to_shipping.order.application.OrderService;
+import com.jumunhasyeotjo.order_to_shipping.order.application.OrderOrchestrator;
 import com.jumunhasyeotjo.order_to_shipping.order.application.command.*;
 import com.jumunhasyeotjo.order_to_shipping.order.application.dto.OrderResult;
 import com.jumunhasyeotjo.order_to_shipping.order.domain.entity.Order;
+import com.jumunhasyeotjo.order_to_shipping.order.presentation.docs.*;
+import com.jumunhasyeotjo.order_to_shipping.order.presentation.dto.request.CancelOrderReq;
 import com.jumunhasyeotjo.order_to_shipping.order.presentation.dto.request.CreateOrderReq;
 import com.jumunhasyeotjo.order_to_shipping.order.presentation.dto.request.OrderUpdateStatusReq;
 import com.jumunhasyeotjo.order_to_shipping.order.presentation.dto.response.CancelOrderRes;
-import com.jumunhasyeotjo.order_to_shipping.order.presentation.dto.response.CompanyOrderItemsRes;
 import com.jumunhasyeotjo.order_to_shipping.order.presentation.dto.response.CreateOrderRes;
 import com.jumunhasyeotjo.order_to_shipping.order.presentation.dto.response.UpdateOrderRes;
+import com.library.passport.annotation.PassportAuthorize;
 import com.library.passport.annotation.PassportUser;
+import com.library.passport.entity.ApiRes;
+import com.library.passport.entity.PassportUserRole;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -21,18 +25,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.UUID;
 
 import static com.library.passport.proto.PassportProto.Passport;
 
+@Tag(name = "Order", description = "주문 관리 (등록, 수정, 삭제, 조회) API")
 @RestController
-@RequestMapping("/v1/orders")
+@RequestMapping("/api/v1/orders")
 @RequiredArgsConstructor
 public class OrderController {
 
-    private final OrderService orderService;
+    private final OrderOrchestrator orderOrchestrator;
 
+    @ApiDocCreateOrder
+    @PassportAuthorize(allowedRoles = PassportUserRole.COMPANY_MANAGER)
     @PostMapping
     public ResponseEntity<ApiRes<CreateOrderRes>> createOrder(@RequestBody @Valid CreateOrderReq req,
                                                               @RequestHeader("x-idempotency-key") String idempotencyKey,
@@ -42,9 +48,12 @@ public class OrderController {
                 UUID.fromString(passport.getBelong()),
                 req.requestMessage(),
                 req.orderProducts(),
-                idempotencyKey);
+                idempotencyKey,
+                req.couponId(),
+                req.tossPaymentKey(),
+                req.tossOrderId());
 
-        Order order = orderService.createOrder(command);
+        Order order = orderOrchestrator.createOrder(command);
         CreateOrderRes res = new CreateOrderRes(order.getId(), order.getStatus());
 
         return ResponseEntity
@@ -52,6 +61,8 @@ public class OrderController {
                 .body(ApiRes.success(res));
     }
 
+    @ApiDocUpdateOrderStatus
+    @PassportAuthorize(allowedRoles = {PassportUserRole.HUB_MANAGER, PassportUserRole.MASTER})
     @PatchMapping("/{orderId}")
     public ResponseEntity<ApiRes<UpdateOrderRes>> updateOrderStatus(@PathVariable UUID orderId,
                                                                     @RequestBody @Valid OrderUpdateStatusReq req,
@@ -64,7 +75,7 @@ public class OrderController {
                 req.status()
         );
 
-        Order order = orderService.updateOrderStatus(command);
+        Order order = orderOrchestrator.updateOrderStatus(command);
         UpdateOrderRes res = new UpdateOrderRes(order.getId(), order.getStatus());
 
         return ResponseEntity
@@ -72,17 +83,22 @@ public class OrderController {
                 .body(ApiRes.success(res));
     }
 
+    @ApiDocCancelOrder
+    @PassportAuthorize(allowedRoles = {PassportUserRole.COMPANY_MANAGER, PassportUserRole.MASTER, PassportUserRole.HUB_MANAGER})
     @DeleteMapping("/{orderId}")
     public ResponseEntity<ApiRes<CancelOrderRes>> deleteOrder(@PathVariable UUID orderId,
-                                                              @PassportUser Passport passport) {
+                                                              @RequestBody CancelOrderReq req,
+                                                              @PassportUser Passport passport)
+    {
         CancelOrderCommand command = new CancelOrderCommand(
                 passport.getUserId(),
                 UUID.fromString(passport.getBelong()),
                 orderId,
-                passport.getRole()
+                passport.getRole(),
+                req.reason()
         );
 
-        Order order = orderService.cancelOrder(command);
+        Order order = orderOrchestrator.cancelOrder(command);
         CancelOrderRes res = new CancelOrderRes(order.getId(), order.getStatus());
 
         return ResponseEntity
@@ -90,6 +106,8 @@ public class OrderController {
                 .body(ApiRes.success(res));
     }
 
+    @ApiDocGetOrder
+    @PassportAuthorize(allowedRoles = {PassportUserRole.COMPANY_MANAGER, PassportUserRole.MASTER, PassportUserRole.HUB_MANAGER})
     @GetMapping("/{orderId}")
     public ResponseEntity<ApiRes<OrderResult>> getOrder(@PathVariable UUID orderId,
                                                         @PassportUser Passport passport) {
@@ -100,18 +118,20 @@ public class OrderController {
                 passport.getRole()
         );
 
-        OrderResult res = orderService.getOrder(command);
+        OrderResult res = orderOrchestrator.getOrder(command);
 
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(ApiRes.success(res));
     }
 
+    @ApiDocSearchOrder
+    @PassportAuthorize(allowedRoles = {PassportUserRole.COMPANY_MANAGER, PassportUserRole.MASTER, PassportUserRole.HUB_MANAGER})
     @GetMapping
     public ResponseEntity<ApiRes<Page<OrderResult>>> searchOrder(@PageableDefault Pageable pageable,
                                                                  @RequestParam(name = "companyId") UUID companyId,
                                                                  @PassportUser Passport passport
-                                                                 ) {
+    ) {
         SearchOrderCommand command = new SearchOrderCommand(
                 passport.getUserId(),
                 UUID.fromString(passport.getBelong()),
@@ -120,20 +140,10 @@ public class OrderController {
                 pageable
         );
 
-        Page<OrderResult> res = orderService.searchOrder(command);
+        Page<OrderResult> res = orderOrchestrator.searchOrder(command);
 
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(ApiRes.success(res));
     }
-
-    @GetMapping("/internal/products")
-    public ResponseEntity<ApiRes<List<CompanyOrderItemsRes>>> getCompanyOrderItems(@RequestParam("companyOrderId") UUID companyOrderId) {
-        List<CompanyOrderItemsRes> res = orderService.getCompanyOrderItems(companyOrderId);
-
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(ApiRes.success(res));
-    }
-
 }
