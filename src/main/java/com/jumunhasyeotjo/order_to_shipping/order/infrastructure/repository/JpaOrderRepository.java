4f6f789674a51d02cdf5f34ee05ba64d1353f724
RaeHwan Park <prh0108@naver.com>
2025-12-09 20:18:18 +0900

    fix: 다중 1:N fetch join 수정

--- src/main/java/com/jumunhasyeotjo/order_to_shipping/order/infrastructure/repository/JpaOrderRepository.java
+++ src/main/java/com/jumunhasyeotjo/order_to_shipping/order/infrastructure/repository/JpaOrderRepository.java
@@ -2,7 +2,8 @@ package com.jumunhasyeotjo.order_to_shipping.order.infrastructure.repository;
 
 import com.jumunhasyeotjo.order_to_shipping.order.domain.entity.Order;
 import com.jumunhasyeotjo.order_to_shipping.order.domain.vo.OrderStatus;
-import com.jumunhasyeotjo.order_to_shipping.order.presentation.dto.response.CompanyOrderItemsRes;
+import com.jumunhasyeotjo.order_to_shipping.order.presentation.dto.response.VendorOrderItemsNameRes;
+import com.jumunhasyeotjo.order_to_shipping.order.presentation.dto.response.VendorOrderItemsRes;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,10 +16,7 @@ import java.util.UUID;
 
 public interface JpaOrderRepository extends JpaRepository<Order, UUID>{
 
-    @Query("SELECT o FROM Order o " +
-            "JOIN FETCH o.orderCompanies oc " +
-            "JOIN FETCH oc.orderProducts " +
-            "WHERE o.id = :orderId")
+    @Query("SELECT o FROM Order o LEFT JOIN FETCH o.vendorOrders WHERE o.id = :orderId")
     Optional<Order> findByIdWithAll(@Param("orderId") UUID orderId);
 
     @Query(value = "select distinct o from Order o " +
@@ -26,12 +24,18 @@ public interface JpaOrderRepository extends JpaRepository<Order, UUID>{
             countQuery = "select count(o) from Order o where o.receiverCompanyId = :companyId")
     Page<Order> findAllByCompanyId(@Param("companyId") UUID companyId, Pageable pageable);
 
-    @Query("SELECT new com.jumunhasyeotjo.order_to_shipping.order.presentation.dto.response.CompanyOrderItemsRes(op.productId, op.quantity) " +
-            "FROM OrderProduct op " +
-            "WHERE op.orderCompany.id = :companyOrderId")
-    List<CompanyOrderItemsRes> findAllByOrderCompany(UUID companyOrderId);
-
     boolean existsByIdempotencyKey(String idempotencyKey);
 
     List<Order> findAllByStatus(OrderStatus orderStatus, Pageable pageable);
+
+    // Internal Query
+    @Query("SELECT new com.jumunhasyeotjo.order_to_shipping.order.presentation.dto.response.VendorOrderItemsRes(op.productId, op.quantity) " +
+            "FROM OrderProduct op " +
+            "WHERE op.vendorOrder.id = :vendorOrderId")
+    List<VendorOrderItemsRes> findAllByVendorOrder(@Param("vendorOrderId") UUID vendorOrderId);
+
+    @Query("SELECT new com.jumunhasyeotjo.order_to_shipping.order.presentation.dto.response.VendorOrderItemsNameRes(op.name, op.quantity) " +
+            "FROM OrderProduct op " +
+            "WHERE op.vendorOrder.id = :vendorOrderId")
+    List<VendorOrderItemsNameRes> findAllByVendorOrderName(@Param("vendorOrderId") UUID vendorOrderId);
 }
