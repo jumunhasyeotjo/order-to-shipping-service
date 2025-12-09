08153b9145f159ea80422f22bb1a068f2cca897f
RaeHwan Park <prh0108@naver.com>
2025-12-09 18:11:04 +0900

    test: 비지니스 로직 변경에 따른 테스트 코드 수정

--- src/test/java/com/jumunhasyeotjo/order_to_shipping/order/domain/VendorOrderDomainTest.java
+++ src/test/java/com/jumunhasyeotjo/order_to_shipping/order/domain/VendorOrderDomainTest.java
@@ -1,7 +1,7 @@
 package com.jumunhasyeotjo.order_to_shipping.order.domain;
 
 import com.jumunhasyeotjo.order_to_shipping.common.exception.BusinessException;
-import com.jumunhasyeotjo.order_to_shipping.order.domain.entity.OrderCompany;
+import com.jumunhasyeotjo.order_to_shipping.order.domain.entity.VendorOrder;
 import com.jumunhasyeotjo.order_to_shipping.order.domain.entity.OrderProduct;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,7 +12,7 @@ import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class OrderCompanyDomainTest {
+public class VendorOrderDomainTest {
 
     @Test
     @DisplayName("주문 업체 정상 프로세스")
@@ -25,12 +25,12 @@ public class OrderCompanyDomainTest {
         String name = "상품";
 
         // when
-        OrderCompany orderCompany = OrderCompany.create(supplierCompanyId, List.of(new OrderProduct(productId, price, quantity, name)));
+        VendorOrder vendorOrder = VendorOrder.create(supplierCompanyId, List.of(new OrderProduct(productId, price, quantity, name)));
 
         // then
-        assertThat(orderCompany).isNotNull();
-        assertThat(orderCompany.getSupplierCompanyId()).isEqualTo(supplierCompanyId);
-        assertThat(orderCompany.getOrderProducts().get(0).getProductId()).isEqualTo(productId);
+        assertThat(vendorOrder).isNotNull();
+        assertThat(vendorOrder.getSupplierCompanyId()).isEqualTo(supplierCompanyId);
+        assertThat(vendorOrder.getOrderProducts().get(0).getProductId()).isEqualTo(productId);
     }
 
     @Test
@@ -44,7 +44,7 @@ public class OrderCompanyDomainTest {
         String name = "상품";
 
         // when & then
-        assertThatThrownBy(() -> OrderCompany.create(supplierCompanyId, List.of(new OrderProduct(productId, price, quantity, name))))
+        assertThatThrownBy(() -> VendorOrder.create(supplierCompanyId, List.of(new OrderProduct(productId, price, quantity, name))))
                 .isInstanceOf(BusinessException.class)
                 .hasMessageContaining("잘못된 요청입니다.");
     }
@@ -56,7 +56,7 @@ public class OrderCompanyDomainTest {
         UUID supplierCompanyId = UUID.randomUUID();
 
         // when & then
-        assertThatThrownBy(() -> OrderCompany.create(supplierCompanyId, List.of()))
+        assertThatThrownBy(() -> VendorOrder.create(supplierCompanyId, List.of()))
                 .isInstanceOf(BusinessException.class)
                 .hasMessageContaining("잘못된 요청입니다.");
     }
