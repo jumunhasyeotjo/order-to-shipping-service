dbe0abaaa894d382de2ea9423dd7a495404a86bb
RaeHwan Park <prh0108@naver.com>
2025-12-09 20:17:46 +0900

    feat: 주문 생성 입력정보 추가

--- src/main/java/com/jumunhasyeotjo/order_to_shipping/order/presentation/dto/request/CreateOrderReq.java
+++ src/main/java/com/jumunhasyeotjo/order_to_shipping/order/presentation/dto/request/CreateOrderReq.java
@@ -4,11 +4,20 @@ import com.jumunhasyeotjo.order_to_shipping.order.application.command.OrderProdu
 import jakarta.validation.constraints.NotEmpty;
 
 import java.util.List;
+import java.util.UUID;
 
 public record CreateOrderReq(
         String requestMessage,
 
         @NotEmpty(message = "주문 상품은 필수 입력값 입니다.")
-        List<OrderProductReq> orderProducts
+        List<OrderProductReq> orderProducts,
+
+        UUID couponId,
+
+        @NotEmpty(message = "tossPaymentKey 는 필수 입력값 입니다.")
+        String tossPaymentKey,
+
+        @NotEmpty(message = "tossOrderId 는 필수 입력값 입니다.")
+        String tossOrderId
 ) {
 }
