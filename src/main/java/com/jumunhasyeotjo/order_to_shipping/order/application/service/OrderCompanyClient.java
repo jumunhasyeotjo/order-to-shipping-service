6b6e337410c3f24ecb0c17a68538b67d760f09e2
RaeHwan Park <prh0108@naver.com>
2025-12-18 15:11:30 +0900

    feat: 로그 추가

--- src/main/java/com/jumunhasyeotjo/order_to_shipping/order/application/service/OrderCompanyClient.java
+++ src/main/java/com/jumunhasyeotjo/order_to_shipping/order/application/service/OrderCompanyClient.java
@@ -1,10 +1,12 @@
 package com.jumunhasyeotjo.order_to_shipping.order.application.service;
 
+import com.jumunhasyeotjo.order_to_shipping.common.aspect.Metric;
 import com.jumunhasyeotjo.order_to_shipping.order.application.dto.ExternalExists;
 
 import java.util.UUID;
 
 public interface OrderCompanyClient {
+    @Metric(value = "존재하는 업체 검증 (외부)", level = Metric.Level.DEBUG)
     ExternalExists existCompany(UUID companyId);
     ExternalExists existCompanyRegionalHub(UUID companyId, UUID hubId);
 }
