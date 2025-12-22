0c88308814d24804d96a00b95b24874d4dcf5285
mmihye <smilehye910@naver.com>
2025-12-22 12:19:29 +0900

    gemini-client mock 처리

--- src/main/java/com/jumunhasyeotjo/order_to_shipping/shipping/infrastructure/external/gemini/GeminiClient.java
+++ src/main/java/com/jumunhasyeotjo/order_to_shipping/shipping/infrastructure/external/gemini/GeminiClient.java
@@ -1,6 +1,7 @@
 package com.jumunhasyeotjo.order_to_shipping.shipping.infrastructure.external.gemini;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -9,6 +10,7 @@ import com.jumunhasyeotjo.order_to_shipping.common.config.OpenFeignConfig;
 import com.jumunhasyeotjo.order_to_shipping.shipping.infrastructure.external.gemini.dto.GeminiRequest;
 import com.jumunhasyeotjo.order_to_shipping.shipping.infrastructure.external.gemini.dto.GeminiResponse;
 
+@Profile("!perf")
 @FeignClient(
 	name = "gemini-client",
 	url = "${gemini.url}",
